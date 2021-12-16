65d5980915dead70eac407463e0ae9e609356728
xavier <xavier.ambrocio@somosoh.pe>
2021-12-16 03:32:21 -0500

    first commit

---
+++ src/main/java/com/ms/practica2/ApiMemSociosCanssandraApplication.java
@@ -0,0 +1,13 @@
+package com.ms.practica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMemSociosCanssandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMemSociosCanssandraApplication.class, args);
+	}
+
+}
